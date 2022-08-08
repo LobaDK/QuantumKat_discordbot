d7c5a8b586d0a259b773ef33f75c80a86ce43f1c
Nicklas <nicklashelsberg@gmail.com>
2022-08-08 14:33:04 +0200

    More code

--- cogs/Main.py
+++ cogs/Main.py
@@ -1,5 +1,6 @@
 import random
 from urllib.parse import urljoin
+from num2words import num2words
 
 import requests
 from bs4 import BeautifulSoup
@@ -10,21 +11,36 @@ class Main(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        @commands.command()
-        async def ar(self, ctx):
-            if ctx.content.startswith("ar"):
-                randomurl('https://aaaa.lobadk.com/', ctx)
-            elif ctx.content.startswith("or"):
-                randomurl('https://possum.lobadk.com/', ctx)
-        def randomurl(url, ctx):
-            links = []
-            reponse = requests.get(url)
-            soup = BeautifulSoup(reponse.text, 'lxml')
-            for link in soup.find_all('a'):
-                temp = link.get('href')
-                if temp.startswith('http') or temp.startswith(',') or temp.startswith('.'):
-                    continue
-                links.append(temp)
-            ctx.send(urljoin(url,random.choice(links)))
+    @commands.command()
+    async def ar(self, ctx):
+        url = 'https://aaaa.lobadk.com/'
+        links = []
+        reponse = requests.get(url)
+        soup = BeautifulSoup(reponse.text, 'lxml')
+        for link in soup.find_all('a'):
+            temp = link.get('href')
+            if temp.startswith('http') or temp.startswith(',') or temp.startswith('.'):
+                continue
+            links.append(temp)
+        await ctx.send(urljoin(url,random.choice(links)))
+
+    @commands.command()
+    async def pr(self, ctx):
+        url = 'https://possum.lobadk.com/'
+        links = []
+        reponse = requests.get(url)
+        soup = BeautifulSoup(reponse.text, 'lxml')
+        for link in soup.find_all('a'):
+            temp = link.get('href')
+            if temp.startswith('http') or temp.startswith(',') or temp.startswith('.'):
+                continue
+            links.append(temp)
+        await ctx.send(urljoin(url,random.choice(links)))
+
+    @commands.command()
+    async def entangle(self, ctx):
+        quantum = ['reality','universe','dimension','timeline']
+        await ctx.send(f'Quantum entangles @{ctx.message.mentions[0]} to the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}')
+
 def setup(bot):
     bot.add_cog(Main(bot))
