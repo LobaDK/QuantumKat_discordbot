6f29abf584af34e1268c89cc1d26244a7d5a8619
Nicklas <nicklashelsberg@gmail.com>
2022-09-21 09:38:06 +0200

    Removed message split

--- cogs/Field.py
+++ cogs/Field.py
@@ -138,7 +138,7 @@ class Field(commands.Cog):
 
     @commands.command(aliases=['as','asearch'])
     async def aaaasearch(self, ctx, arg=""):
-        if len(arg) >= 2:
+        if len(arg) >= 4:
             allowed = re.compile('^(\.?)[a-zA-Z0-9]+(\.?)$')
             if allowed.match(arg):
                 response = requests.get(f'https://aaaa.lobadk.com/?search={arg}')
@@ -154,27 +154,13 @@ class Field(commands.Cog):
                 if len(links) == 0:
                     await ctx.send('Search returned nothing')
                 else:
-                    links = ' '.join(links)
-                    if len(links) < 4000:
-                        await ctx.send(links)
+                    if len(' '.join(links)) > 4000:
+                        await ctx.send('Too many results! Try narrowing down the search')
                     else:
-                        limit = 4000
-                        lines = links.splitlines()
-                        new_message = ''
-                        message_list = []
-                        for line in lines:
-                            if len(new_message+line+'\n') <= limit:
-                                new_message += line+'\n'
-                            else:
-                                message_list.append(new_message)
-                                new_message = ''
-                        if new_message:
-                            message_list.append(new_message)
-                        await ctx.send(message_list)
-                        
+                        await ctx.send(' '.join(links))
             else:
-                await ctx.send('At least two alphanumeric characters are required, or 1 alphanumeric character and a `.` at the start or end')
+                await ctx.send('At least three alphanumeric characters are required, and only `.` is allowed')
         else:
-            await ctx.send('Search too short! A minimum of 2 characters are required')
+            await ctx.send('Search too short! A minimum of 4 characters are required')
 def setup(bot):
     bot.add_cog(Field(bot))
