c1a0b9b76d173a42a3b086b026d9516a17bdce4c
LobaDK <lobadk@lobadk.com>
2022-09-27 21:30:07 +0200

    2.0 now fully works

--- QuantumKat.py
+++ QuantumKat.py
@@ -5,37 +5,29 @@ from num2words import num2words
 import discord
 from discord.ext import commands
 
-client = discord.Client()
+with open('./files/token', 'r') as tokenfile:
+    token = tokenfile.read().strip()
 
-async def main():
-
-    with open('./files/token', 'r') as tokenfile:
-        token = tokenfile.read().strip()
-    
-
-    intents = discord.Intents.all()
-
-    bot = commands.Bot(command_prefix='?', help_command=None, intents=intents)
-
-    asyncio.run(setup(bot))
-
-    async with client:
-        await client.start(token)
-
-    @bot.check
-    async def globally_block_dms(ctx):
-        return ctx.guild is not None
-
-    @bot.event
-    async def on_ready():
-        quantum = ['reality','universe','dimension','timeline']
-        print(f'{bot.user} has appeared from the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
-        #channel = bot.get_channel(873703927621758986)
-        #await channel.send(f'QuantumKat has entered a state of superposition in the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
-
+initial_extensions = ['cogs.Field','cogs.Entanglement','cogs.Tunnel', 'cogs.Activity']
 async def setup(bot):
-    initial_extensions = ['cogs.Activity']
     for extension in initial_extensions:
         await bot.load_extension(extension)
+    await bot.start(token)
 
-asyncio.run(main())
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', help_command=None, intents=intents)
+
+@bot.check
+async def globally_block_dms(ctx):
+    return ctx.guild is not None
+
+@bot.event
+async def on_ready():
+    quantum = ['reality','universe','dimension','timeline']
+    print(f'{bot.user} has appeared from the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
+    #channel = bot.get_channel(873703927621758986)
+    #await channel.send(f'QuantumKat has entered a state of superposition in the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
+
+asyncio.run(setup(bot))
