58a5708cb7a191bd918ab0ac731e4bf20cd52947
Nicklas <nicklashelsberg@gmail.com>
2022-09-27 14:57:19 +0200

    Testing further rework

--- QuantumKat.py
+++ QuantumKat.py
@@ -5,28 +5,37 @@ from num2words import num2words
 import discord
 from discord.ext import commands
 
-with open('./files/token', 'r') as tokenfile:
-    token = tokenfile.read().strip()
+client = discord.Client()
 
-initial_extensions = ['cogs.Activity']
+async def main():
+
+    with open('./files/token', 'r') as tokenfile:
+        token = tokenfile.read().strip()
+    
+
+    intents = discord.Intents.all()
+
+    bot = commands.Bot(command_prefix='?', help_command=None, intents=intents)
+
+    asyncio.run(setup(bot))
+
+    async with client:
+        await client.start(token)
+
+    @bot.check
+    async def globally_block_dms(ctx):
+        return ctx.guild is not None
+
+    @bot.event
+    async def on_ready():
+        quantum = ['reality','universe','dimension','timeline']
+        print(f'{bot.user} has appeared from the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
+        #channel = bot.get_channel(873703927621758986)
+        #await channel.send(f'QuantumKat has entered a state of superposition in the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
+
 async def setup(bot):
+    initial_extensions = ['cogs.Activity']
     for extension in initial_extensions:
         await bot.load_extension(extension)
 
-intents = discord.Intents.all()
-
-bot = commands.Bot(command_prefix='?', help_command=None, intents=intents)
-
-@bot.check
-async def globally_block_dms(ctx):
-    return ctx.guild is not None
-
-@bot.event
-async def on_ready():
-    quantum = ['reality','universe','dimension','timeline']
-    print(f'{bot.user} has appeared from the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
-    #channel = bot.get_channel(873703927621758986)
-    #await channel.send(f'QuantumKat has entered a state of superposition in the {num2words(random.randint(1,1000), to="ordinal_num")} {random.choice(quantum)}!')
-
-asyncio.run(setup(bot))
-bot.run(token)
+asyncio.run(main())
