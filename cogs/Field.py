a576561f7c452faa3527154a1118e6610d7a48af
Nicklas <nicklashelsberg@gmail.com>
2022-11-08 09:14:50 +0100

    Added correct path to file counter

--- cogs/Field.py
+++ cogs/Field.py
@@ -261,7 +261,7 @@ class Field(commands.Cog):
         extensions = ['*.gif', '*.jpeg', '*.jpg', '*.mov', '*.mp3', '*.mp4', '*.png', '*.webm', '*.webp']
         all_files = []
         for extension in extensions:
-            current_extension = glob.glob(extension)
+            current_extension = glob.glob(f'/var/www/aaaa/{extension}')
             all_files += current_extension
         await ctx.send(f'There are currently {len(all_files)} quantised datasets.')
 
