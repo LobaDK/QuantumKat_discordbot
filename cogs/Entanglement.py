58299cb679c202b9115f49d47e8e8d237209ff5e
Nicklas <nicklashelsberg@gmail.com>
2022-11-22 08:11:28 +0100

    Updated update command and added improved version check

--- cogs/Entanglement.py
+++ cogs/Entanglement.py
@@ -393,53 +393,72 @@ class Entanglement(commands.Cog):
     @commands.command(brief="(Bot owner only) Fetches new updates and reloads all changed/updated cogs/extensions.", description="Fetches the newest version by running 'git pull' and then reloads the cogs/extensions if successful.")
     @commands.is_owner()
     async def update(self, ctx):
+
+        #Attempt to get the current commit HASH before updating
         try:
-            process = await asyncio.create_subprocess_shell('git pull', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            process1 = await asyncio.create_subprocess_shell('git rev-parse --short HEAD', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        except Exception as e:
+            print('{}: {}'.format(type(e).__name__, e))
+            await ctx.send('Error getting current Git information')
+            return
+
+        stderr1, stdout1 = await process1.communicate()
+
+        current_version = stderr1.decode().replace('\n', '')
+
+        try:
+            process2 = await asyncio.create_subprocess_shell('git pull', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
             
             #DO NOT CHANGE ORDER
             #Git's output seems to be reversed, and most information gets piped to STDERR instead for some reason
             #STDOUT may also sometimes contain either the data, or some other random data, or part of the whole output, from STDERR
             #"Aready up to date" is piped to STDUT, but output from file changes when doing "git pull" for example
             #Are outputted to STDERR instead, hence why it is also reversed here
-            stderr, stdout = await process.communicate()
+            stderr2, stdout2 = await process2.communicate()
        
         except Exception as e:
             print('{}: {}'.format(type(e).__name__, e))
-            await ctx.send('Error running command')
+            await ctx.send('Error running update')
+            return
 
         #For some reason after decoding Git's output stream, "b'" and "\\n'" shows up everywhere in the output
         #This removes any of them, cleaning up the output Test
-        stdout = stdout.decode()
-        stdout = stdout.replace("b'","")
-        stdout = stdout.replace("\\n'","")
-        stdout = stdout.replace("\n'","")
-        stderr = stderr.decode()
-        stderr = stderr.replace("b'","")
-        stderr = stderr.replace("\\n'","")
-        stderr = stderr.replace("\n'","")
+        stdout2 = stdout2.decode()
+        stdout2 = stdout2.replace("b'","")
+        stdout2 = stdout2.replace("\\n'","")
+        stdout2 = stdout2.replace("\n'","")
+        stderr2 = stderr2.decode()
+        stderr2 = stderr2.replace("b'","")
+        stderr2 = stderr2.replace("\\n'","")
+        stderr2 = stderr2.replace("\n'","")
 
         #For some reason Git on Windows returns the string without hyphens, while Linux returns it with hyphens
-        if 'Already up to date' in stderr or 'Already up-to-date' in stderr:
-            await ctx.send(stderr)
+        if 'Already up to date' in stderr2 or 'Already up-to-date' in stderr2:
+            await ctx.send(stderr2)
         
-        elif stderr:
+        elif stderr2:
 
             #Send the output of Git, which displays whichs files has been updated, and how much
             #Then sleep 2 seconds to allow the text to be sent, and read
-            await ctx.send(stderr)
+            await ctx.send(stderr2)
             await asyncio.sleep(2)
 
-            #This command display the filenames of the files the changed between the last and current versions
-            process2 = await asyncio.create_subprocess_shell('git diff --name-only HEAD~1 HEAD', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-
+            #Attempt to get a list of files that changed between the pre-update version, using the previously required HASH, and now
+            try:
+                process3 = await asyncio.create_subprocess_shell(f'git diff --name-only {current_version} HEAD', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            except Exception as e:
+                print('{}: {}'.format(type(e).__name__, e))
+                await ctx.send('Error running file-change check. Manual reloading required')
+                return
+            
             #Save the output (filenames) in stdout2
-            stderr1, stdout2 = await process2.communicate()
+            stderr3, stdout3 = await process3.communicate()
 
             #Decode and remove "b'" characters
-            stderr1 = stderr1.decode().replace("b'","")
+            stderr3 = stderr3.decode().replace("b'","")
 
             #Each displayed file is on a newline, so split by the newlines to save them as a list
-            extensions = stderr1.split('\n')
+            extensions = stderr3.split('\n')
 
             #Iterate through each listed files
             for extension in extensions:
@@ -460,8 +479,8 @@ class Entanglement(commands.Cog):
                         print('{}: {}'.format(type(e).__name__, e))
                         await ctx.send(f'successfully loaded {os.path.basename(extension)[:-3]}, but no setup was found!')
         
-        elif stdout:
-            await ctx.send(stdout)
+        elif stdout2:
+            await ctx.send(stdout2)
         
 
 ######################################################################################################
