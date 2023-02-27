b17c994399909c455abe925b5d726501922866f9
LobaDK <lobadk@lobadk.com>
2023-02-27 20:25:49 +0100

    Same fix but actually correct this time

--- cogs/Field.py
+++ cogs/Field.py
@@ -143,8 +143,8 @@ class Field(commands.Cog):
     @commands.command(aliases=['rockpaperscissor'], brief='Rock, Paper, Scissors game.', description='A Rock, Paper, Scissors game. Supports one argument, being the chosen gesture. Emote is also supported. Is not rigged.')
     async def rps(self, ctx, *, item=""):
         substring = ['rock','rocks','paper','papers','scissor','scissors','✂️','🪨','🧻']
+        item = item.lower()
         if any(_ in item for _ in substring):
-            item = item.lower()
             whowin = random.choices(['I win!','You win!'], k=1, weights=[100,5])[0]
             if item == 'scissor':
                 item = item + 's'
