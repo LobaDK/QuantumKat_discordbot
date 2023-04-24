a662820997b1877d417b533952587d5e06a4a000
Nicklas <gta5.r6s.bf4@gmail.com>
2023-04-24 08:18:50 +0200

    Swapped status command to use reply

--- Entanglements/StatusCommand.py
+++ Entanglements/StatusCommand.py
@@ -1,7 +1,7 @@
 from psutil import cpu_percent, virtual_memory, disk_usage
 
 async def StatusCommand(self, ctx):
-    await ctx.send(f'''
+    await ctx.reply(f'''
 Current CPU Usage: {cpu_percent(1)}%
 RAM: {int(virtual_memory().used / 1024 / 1024)}MB / {int(virtual_memory().total / 1024 / 1024)}MB | {virtual_memory().percent}%
 Primary disk: {int(disk_usage('/').used / 1024 / 1024 / 1000)}GB / {int(disk_usage('/').total / 1024 / 1024 / 1000)}GB | {disk_usage('/').percent}%
