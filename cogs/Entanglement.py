05114f3cb8fcf0b38b588f6476e85ed3436ec292
LobaDK <gta5.r6s.bf4@gmail.com>
2023-04-26 11:01:57 +0200

    Added --no-playlist flag when using yt-dlp

--- cogs/Entanglement.py
+++ cogs/Entanglement.py
@@ -159,11 +159,11 @@ class Entanglements(commands.Cog):
             if mode.upper() == 'YT':
                 
                 #Disallow playlists
-                if not "&list=" in URL and not "playlist" in URL:
+                if not "playlist" in URL:
                 
                     #Download the best (up to 720p) MP4 video and m4a audio, and then combines them
                     #Or a single video with audio included (up to 720p), if that's the best option
-                    arg = f'yt-dlp -f bv[ext=mp4]["height<=720"]+ba[ext=m4a]/b[ext=mp4]["height<=720"] "{URL}" -o "{data_dir}{filename}.%(ext)s"'
+                    arg = f'yt-dlp -f bv[ext=mp4]["height<=720"]+ba[ext=m4a]/b[ext=mp4]["height<=720"] "{URL}" --no-playlist -o "{data_dir}{filename}.%(ext)s"'
                     
                     msg = await ctx.reply('Creating quantum tunnel... Tunnel created! Quantizing data...', silent=True)
                         
