5dedaec28dfd17882971effe4d02d816b8af4e6d
Nicklas H <gta5.r6s.bf4@gmail.com>
2024-03-18 09:58:33 +0100

    Fixed OpenAI API key check and initialization

--- cogs/Chat.py
+++ cogs/Chat.py
@@ -1,6 +1,7 @@
 from openai import AsyncOpenAI as OpenAI, OpenAIError
 import logging
 import tiktoken
+import os
 
 from discord.ext import commands
 
@@ -10,10 +11,13 @@ logger = logging.getLogger('discord')
 class Chat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.openai = OpenAI()
         self.encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
         self.system_message = "You are QuantumKat, a cybernetic cat with void-like black fur. You have the intelligence of a human and can talk. You can teleport to any point in time or location between dimensions, realities, universes and timelines. while your past is unknown and you have no knowledge of how you came to existence, you know you have to use these powers to monitor and keep them all safe, however, you are still young and don't quite know how to do it correctly, as well as being clumsy, often resulting in you appearing in an incorrect location or doing the wrong thing. You are slightly sarcastic."
-        if self.openai.api_key is None:
+        if os.environ.get('OPENAI_API_KEY'):
+            self.FOUND_API_KEY = True
+            self.openai = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
+        else:
+            self.FOUND_API_KEY = False
             logger.error("OpenAI API key not found. Chat commands will not work.")
 
     def calculate_tokens(self, user_message: str) -> int:
@@ -26,13 +30,13 @@ class Chat(commands.Cog):
 
     @commands.command(aliases=['chat', 'talk'], brief='Talk to QuantumKat.', description='Talk to QuantumKat using the OpenAI API/ChatGPT.')
     async def Chat(self, ctx: commands.Context, *, user_message=""):
-        if OpenAI.api_key is not None:
+        if self.FOUND_API_KEY is True:
             if user_message:
                 tokens = self.calculate_tokens(user_message)
                 if not tokens > 256:
                     async with ctx.typing():
                         try:
-                            response = await OpenAI.chat.completions.create(
+                            response = await self.openai.chat.completions.create(
                                 model="gpt-3.5-turbo",
                                 messages=[
                                     {
@@ -63,6 +67,7 @@ class Chat(commands.Cog):
             await ctx.send("OpenAI API key not found. Chat commands will not work.")
 
     logger.info("Started Chat!")
+    print("Started Chat!")
 
 
 async def setup(bot):
