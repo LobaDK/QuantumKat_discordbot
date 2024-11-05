d5152a9f139534f519e44849e96d4a102dcf6855
Nicklas <gta5.r6s.bf4@gmail.com>
2024-11-05 13:23:41 +0100

    Refactor main.py: implement thread management for QuantumKat bot with restart functionality

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-from threading import Thread
+from threading import Thread, Event
 
 from QuantumKat.quantum_kat import quantum_bot
 
+threads: list[Thread] = []
+restart_quantum_kat_event = Event()
+
+
 if __name__ == "__main__":
-    quantum_kat_thread = Thread(target=quantum_bot.run)
-    quantum_kat_thread.start()
-    quantum_kat_thread.join()
+    threads.append(Thread(target=quantum_bot.run, name="QuantumKat"))
+
+    for thread in threads:
+        thread.start()
+
+    while True:
+        # Wait for the event to be set, then reset it
+        restart_quantum_kat_event.wait()
+        restart_quantum_kat_event.clear()
+
+        for thread in [t for t in threads if t.name == "QuantumKat"]:
+            thread.join()  # Wait for the thread to finish
+            threads.remove(thread)
+
+        # Create a new thread and start it
+        new_thread = Thread(target=quantum_bot.run, name="QuantumKat")
+        new_thread.start()
+        threads.append(new_thread)
